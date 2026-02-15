1ece3cda0d3ea6ff562c358cccfa5140334aa261
Solomon Mathebula <smathebula@getabyte.co.za>
2026-02-15 22:57:51 +0200

    Favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,18 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Getabyte Tech',
-  description: 'Cloud | Cybersecurity | AI | Software Consulting',
+  title: 'Getabyte',
+  description: 'Software Consulting',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
+       <head>
+    <link rel="icon" href="/images/favi.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favi.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+  </head>
       <body className={`${inter.className} bg-white text-black`}>
       
         <Toaster position="top-right" toastOptions={{ style: { background: "#1e3a8a", color: "#fff" } }} />
